2a166b9d41f3b9a95b4e830da3c1720c050f7988
pdo <pdo@a08b7396-6d28-496f-9e4f-967280088352>
2013-12-14 21:20:44 +0000

    reverted gamescreen to initial state

--- skeleton/src/com/gamedev/ld28/screens/GameScreen.java
+++ skeleton/src/com/gamedev/ld28/screens/GameScreen.java
@@ -1,25 +1,20 @@
 package com.gamedev.ld28.screens;
 
-import java.util.ArrayList;
+import aurelienribon.tweenengine.TweenManager;
 
 import com.badlogic.gdx.Screen;
 import com.gamedev.ld28.Skeleton;
-import com.gamedev.ld28.entities.Entity;
 
-public abstract class GameScreen implements Screen
-{
+public abstract class GameScreen implements Screen {
+
+	public static final TweenManager tweenMgr = new TweenManager();
+
 	protected Skeleton game;
-        protected ArrayList<Entity> entities;
 
-	public GameScreen(Skeleton game)
-        {
-          this.game = game;
-          entities = new ArrayList<Entity>();
-        }
+	public GameScreen(Skeleton game) { this.game = game; }
 
-	public void update(float delta)
-        {
-
+	public void update(float delta) {
+		tweenMgr.update(delta);
 	}
 
 	@Override
