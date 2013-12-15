ad7d4ff53f803c89fc3becec1f9393f1c848d6b5
imcnamara <33228937+icmcnamara@users.noreply.github.com>
2013-12-15 00:59:10 +0000

    Added Barrel.  Updated move functions in base class.

---
+++ skeleton/src/com/gamedev/ld28/entities/Barrel.java
@@ -0,0 +1,19 @@
+package com.gamedev.ld28.entities;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.gamedev.ld28.Assets;
+import com.gamedev.ld28.utils.Constants;
+
+public class Barrel extends Entity {
+	
+	public Barrel(int x, int y) {
+		super(x, y, Constants.NORTH);
+		sprite = new Sprite(Assets.barrel);
+	}
+	
+	public void push(int dir) {
+		super.saveState();
+		super.movePush(dir);
+	}
+	
+}
