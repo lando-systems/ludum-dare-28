0672ab29c399c87a343b66c8330a52140c3d961b
pdo <pdo@a08b7396-6d28-496f-9e4f-967280088352>
2013-12-15 03:07:03 +0000

    pushin them barrels

--- skeleton/src/com/gamedev/ld28/entities/Wizard.java
+++ skeleton/src/com/gamedev/ld28/entities/Wizard.java
@@ -1,18 +1,17 @@
 package com.gamedev.ld28.entities;
-
+import com.gamedev.ld28.Level;
 import com.gamedev.ld28.Assets;
 
-public class Wizard extends Entity {
-	
-	public Wizard(int x, int y, int dir) {
-		super(x, y, dir);
+public class Wizard extends Entity
+{
+	public Wizard(Level level, int x, int y, int dir) {
+		super(level, x, y, dir);
 		buildAnim(Assets.wizardsheet);
 	}
 	
 	@Override
 	public void takeAction(Entity.ACTIONS action) {
-		super.saveState();
-		super.doAction(action);
+		super.moveAction(action);
 	}
 
 }
