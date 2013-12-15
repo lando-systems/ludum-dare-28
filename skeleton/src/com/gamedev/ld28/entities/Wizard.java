086b1c39f3783ad1b3d1856b5ca751f9d0c3e2ba
dsgraham <dsgraham@a08b7396-6d28-496f-9e4f-967280088352>
2013-12-15 00:53:22 +0000

    wall textures!

--- skeleton/src/com/gamedev/ld28/entities/Wizard.java
+++ skeleton/src/com/gamedev/ld28/entities/Wizard.java
@@ -1,9 +1,12 @@
 package com.gamedev.ld28.entities;
 
+import com.gamedev.ld28.Assets;
+
 public class Wizard extends Entity {
 	
 	public Wizard(int x, int y, int dir) {
 		super(x, y, dir);
+		buildAnim(Assets.zombiesheet);
 	}
 	
 	@Override
